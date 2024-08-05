ed6361c30eead9f622b7a21ec04d25756bae4f3a
nhlee98 <e5slnh9876@gmail.com>
2024-08-05 19:27:10 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/dongbaeb/demo/DongbaebApplication.java
@@ -0,0 +1,13 @@
+package com.dongbaeb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongbaebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongbaebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dongbaeb/demo/DongbaebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongbaeb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongbaebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
