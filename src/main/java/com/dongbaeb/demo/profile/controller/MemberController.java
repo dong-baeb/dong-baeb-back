d7b368afa94743b087a5bfa8896185d754b29678
limstar113 <limstar113@naver.com>
2024-08-11 03:56:13 +0900

    feat: 사용자 프로필 구현(수정2차)

--- src/main/java/com/dongbaeb/demo/profile/controller/MemberController.java
+++ src/main/java/com/dongbaeb/demo/profile/controller/MemberController.java
@@ -7,8 +7,6 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
-
 @RestController
 @RequestMapping("/members")
 public class MemberController {
