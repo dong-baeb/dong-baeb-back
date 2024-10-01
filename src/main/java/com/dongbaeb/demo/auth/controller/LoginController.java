693294f629e228841030591756196972a7f5eafb
LSe-Yeong <150990620+LSe-Yeong@users.noreply.github.com>
2024-10-01 11:14:51 +0900

    feat: Swagger 의존성 주입 및 컨트롤러에 Swagger 문서화 작업 (#17)
    스웨거 UI Develop으로 merge

--- src/main/java/com/dongbaeb/demo/auth/controller/LoginController.java
+++ src/main/java/com/dongbaeb/demo/auth/controller/LoginController.java
@@ -1,19 +1,26 @@
 package com.dongbaeb.demo.auth.controller;
 
 import com.dongbaeb.demo.auth.service.LoginService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "로그인 컨트롤러", description = "User에게 Request를 받아 로그인과 관련된 로직을 수행한다.")
 @RequiredArgsConstructor
 @RestController
 public class LoginController {
     private final LoginService loginService;
 
+    @Operation(summary = "code를 받아 로그인을 수행", description = "Kakao로 부터 인가 code를 받고 이를 이용하여 로그인을 진행한다.")
     @GetMapping("/oauth-callback")
-    public ResponseEntity<String> login(@RequestParam(name = "code") String code) {
+    public ResponseEntity<String> login(
+            @Parameter(description = "Kakao로 부터 받은 인가 code")
+            @RequestParam(name = "code") String code) {
         return ResponseEntity.ok(loginService.login(code));
     }
 }
