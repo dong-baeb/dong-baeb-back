3fdf06c58cb437bfc2171840f49b9f0085e146bb
이낙헌 <95845037+nak-honest@users.noreply.github.com>
2024-10-16 21:01:42 +0900

    <feat> dev 용 application.yml 파일 작성 (#21)
    * chore: 사용하지 않는 설정 제거
    
    * chore: local, dev 프로필 분리
    
    * chore: 깃허브 액션에 env 설정 추가
    
    * chore: application.yml 수정
    
    * chore: test 프로필 추가

--- src/test/java/com/dongbaeb/demo/DongbaebApplicationTests.java
+++ src/test/java/com/dongbaeb/demo/DongbaebApplicationTests.java
@@ -2,12 +2,14 @@ package com.dongbaeb.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DongbaebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
