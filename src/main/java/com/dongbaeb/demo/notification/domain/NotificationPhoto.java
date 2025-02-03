79b991cf47f8353ed34988b2b56d37ca170a4c49
nhlee98 <e5slnh9876@gmail.com>
2025-02-03 20:45:56 +0900

    feat: 공지 사진 엔티티 추가

---
+++ src/main/java/com/dongbaeb/demo/notification/domain/NotificationPhoto.java
@@ -0,0 +1,33 @@
+package com.dongbaeb.demo.notification.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class NotificationPhoto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "notification_id", nullable = false)
+    private Notification notification;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    public NotificationPhoto(Notification notification, String imageUrl) {
+        this.notification = notification;
+        this.imageUrl = imageUrl;
+    }
+}
