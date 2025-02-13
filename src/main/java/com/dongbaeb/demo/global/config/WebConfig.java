8eb199c28723e9da8fdb0e04a306c361a2811e85
nhlee98 <e5slnh9876@gmail.com>
2025-02-14 00:29:39 +0900

    fix: MemberAuthArgumentResolver 등록

--- src/main/java/com/dongbaeb/demo/global/config/WebConfig.java
+++ src/main/java/com/dongbaeb/demo/global/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.dongbaeb.demo.global.config;
 
 import com.dongbaeb.demo.global.KakaoUserInfoArgumentResolver;
+import com.dongbaeb.demo.global.MemberAuthArgumentResolver;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +12,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private final KakaoUserInfoArgumentResolver kakaoUserInfoArgumentResolver;
+    private final MemberAuthArgumentResolver memberAuthArgumentResolver;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(kakaoUserInfoArgumentResolver);
+        resolvers.addAll(List.of(kakaoUserInfoArgumentResolver, memberAuthArgumentResolver));
     }
 }
