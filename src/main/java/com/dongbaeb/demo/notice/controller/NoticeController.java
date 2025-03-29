e3701809f0d1734d893a2089f1019d0e08f41135
Lse-Yeong <ocdee39@gmail.com>
2025-03-29 22:56:36 +0900

    refactor: 스웨거 문서 추가

--- src/main/java/com/dongbaeb/demo/notice/controller/NoticeController.java
+++ src/main/java/com/dongbaeb/demo/notice/controller/NoticeController.java
@@ -5,6 +5,9 @@ import com.dongbaeb.demo.member.domain.University;
 import com.dongbaeb.demo.notice.domain.NoticeCategory;
 import com.dongbaeb.demo.notice.dto.NoticeResponse;
 import com.dongbaeb.demo.notice.service.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Notice 전체 조회 관련 API 요청", description = "Notice 공지 전체 목록 조회 등의 역할을 한다.")
 @RequiredArgsConstructor
 @RequestMapping("/notifications")
 @RestController
@@ -34,11 +38,16 @@ public class NoticeController {
 //        return response;
 //    }
 
+    @Operation(summary = "조건에 맞는 공지를 가져온다.", description = "category와 name에 맞는 공지를 가져온다. page, size를 통해 가져오는 양을 조절할 수 있다.")
     @GetMapping("/")
-    public ResponseEntity<List<NoticeResponse>> getNotices(@RequestParam("category") NoticeCategory noticeCategory,
-                                                           @RequestParam("name") University name,
-                                                           Pageable pageable,
-                                                           MemberAuth memberAuth) {
+    public ResponseEntity<List<NoticeResponse>> getNotices(
+            @Parameter(description = "공지 카테고리 정보")
+            @RequestParam("category") NoticeCategory noticeCategory,
+            @Parameter(description = "조회 하고 싶은 학교 이름")
+            @RequestParam("name") University name,
+            Pageable pageable,
+            MemberAuth memberAuth) {
+
         List<NoticeResponse> notices;
 
         if (NoticeCategory.EAST_SEOUL.equals(noticeCategory)) {
